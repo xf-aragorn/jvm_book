c50c3f40011c29517b3ac7a5fe3c25f1f6deeff1
ARAGORNHW\xufeng <xm_caiyun@163.com>
2021-06-27 02:10:52 +0800

    新增了参数模式

--- src/org/fenixsoft/jvm/chapter3/Chapter3Main.java
+++ src/org/fenixsoft/jvm/chapter3/Chapter3Main.java
@@ -3,6 +3,7 @@ package org.fenixsoft.jvm.chapter3;
 public class Chapter3Main {
 
     //vm args: -verbose:gc -XX:+UseSerialGC -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+    //vm args: -verbose:gc -XX:+UseSerialGC -Xms20M -Xmx20M -Xmn10M -XX:+PrintGC -XX:SurvivorRatio=8  //打印粗略信息
     public static void main(String[] args){
         TestSerialGCAllocation.testAllocation();
     }
